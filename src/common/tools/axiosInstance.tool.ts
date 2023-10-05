47d43dd522d39b119dbf0582b079abc5dc0d6647
ybchar <uiurihappy@sju.ac.kr>
2023-10-05 10:55:40 +0900

    fix: axios

--- src/common/tools/axiosInstance.tool.ts
+++ src/common/tools/axiosInstance.tool.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: `${process.env.VITE_API_URL}`, // 백엔드 서버 주소
+  baseURL: `https://ybchar.shop`, // 백엔드 서버 주소
 });
 
 export default instance;
